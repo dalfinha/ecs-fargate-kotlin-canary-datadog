ef6ea1db181ee67c91b45b75ed4cf01f877e6dfa
Dalva <72402847+dalfinha@users.noreply.github.com>
2025-04-24 22:00:32 -0300

    feat: automatizando local a infraestrutura com terraform (#2) (#3)
    * feat: estrutura de pastas
    
    * feat: organizacao por modules
    
    * featÃ: provider config validate
    
    * feat: config task definition default
    
    * feat: configs - ecs cluster, ecs service, ecs task definition
    
    * feat: config data, outputs | refactor: arquivos modules
    
    * feat: gitignore terraform performing
    
    * test: eficiencia - gitignore pos init local
    
    * fix: access data iam task definition
    
    * fix: access data iam ecs service
    
    * fix: task definition, variables type, sg default list
    
    * featÃ: add log group for tdefintion
    
    * fix: variables default add - log group default

---
+++ infra/data.tf
@@ -0,0 +1,24 @@
+data "aws_iam_role" "this" {
+  name = "ecs-tasks-demo"
+}
+
+data "aws_ecr_image" "this" {
+  repository_name = "demo/kotlin-app-canary"
+  most_recent     = true
+}
+
+data "aws_vpc" "this" {
+  default = true
+}
+
+data "aws_security_group" "this" {
+  name   = "default"
+  vpc_id = data.aws_vpc.this.id
+}
+
+data "aws_subnets" "this" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.this.id]
+  }
+}
---
+++ infra/ecs_cluster.tf
@@ -0,0 +1,10 @@
+resource "aws_ecs_cluster" "this" {
+  depends_on = [aws_cloudwatch_log_group.this]
+  name = var.ecs_cluster_name
+
+  setting {
+    name  = "containerInsights"
+    value = var.env == "dev" ? "disabled" : "enabled"
+  }
+
+}
