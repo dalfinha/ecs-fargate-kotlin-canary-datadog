566b1f2e1f855a467fce5b1ae27e86a5b3d43d47
Dalva <72402847+dalfinha@users.noreply.github.com>
2025-05-08 20:54:41 -0300

    feat: task definition vars

--- infra/ecs-service/variables.tf
+++ infra/ecs-service/variables.tf
@@ -2,3 +2,14 @@ variable "ecs_cluster_name" {}
 
 variable "env" {}
 
+variable "service_name" {}
+
+variable "role_task_arn" {}
+
+variable "role_eecution_arn" {}
+
+variable "port_application" {}
+
+variable "env_variable" {
+  type = list(string)
+}
