ff9ae33d8df36b0058f4260aa5ed393e96b44a0a
Dalva <dalva.mariana.dm@gmail.com>
2025-06-01 22:12:29 -0300

    test: var in locals datadog

--- infra/ecs-service/service_task_definition.tf
+++ infra/ecs-service/service_task_definition.tf
@@ -18,7 +18,7 @@ resource "aws_ecs_task_definition" "this" {
   container_definitions = jsonencode(
     concat(
       [
-        {""
+        {
           name  = "container-${var.service_name}"
           image = var.uri_image
           cpu   = 256
