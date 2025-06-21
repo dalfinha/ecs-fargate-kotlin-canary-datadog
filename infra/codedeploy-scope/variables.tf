0b17158ee080c9105aed98415bd8e63c93ef875a
Dalva <dalva.mariana.dm@gmail.com>
2025-06-21 19:21:35 -0300

    feat: description variables

--- infra/codedeploy-scope/variables.tf
+++ infra/codedeploy-scope/variables.tf
@@ -3,30 +3,26 @@ variable "role_codedeploy" {
 }
 
 variable "deployment_config_canary" {
+  description = "Estratégia de Canary associada a aplicação"
   default = "CodeDeployDefault.ECSCanary10Percent5Minutes"
 }
 
 variable "cluster_name" {
   description = "Nome do Cluster Existente"
-  default = "meu-cluster-name"
 }
 
 variable "service_name" {
   description = "Nome do Service Existente"
-  default = "meu-service-name"
 }
 
 variable "cluster_arn" {
-  description = "ARN do Cluster"
-  default = "cluster_arn"
+  description = "ARN do Cluster ECS Fargate"
 }
 
 variable "target_group" {
-  description = "Nome do Target Group"
-  default = "seu-target-group"
+  description = "Nome do Target Group que será associado ao serviço. OBS: Não adicionar o -blue ou -green"
 }
 
 variable "port_application" {
-  description = "Porta da aplicação"
-  default = 10000
+  description = "Porta da Aplicação"
 }
