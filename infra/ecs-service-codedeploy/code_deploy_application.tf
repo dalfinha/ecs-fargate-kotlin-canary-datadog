bf707d2bb45ea552b2710babd60d2be5558480ab
Dalva <dalva.mariana.dm@gmail.com>
2025-07-09 20:33:34 -0300

    feat: remodularização - teste de mesa - ok

--- infra/ecs-service-codedeploy/code_deploy_application.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_codedeploy_app" "this" {
-  depends_on = [aws_ecs_service.this]
-  compute_platform = "ECS"
-  name             = "app-${var.service_name}"
-}
--- infra/ecs-service-codedeploy/service_autoscaling_target.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_appautoscaling_target" "ecs_service_scaling" {
-  depends_on = [ aws_ecs_service.this ]
-
-  max_capacity       = local.max_capacity_scaling
-  min_capacity       = aws_ecs_service.this.desired_count
-  resource_id        = "service/${aws_ecs_cluster.this.name}/${aws_ecs_service.this.name}"
-  scalable_dimension = "ecs:service:DesiredCount"
-  service_namespace  = "ecs"
-}
